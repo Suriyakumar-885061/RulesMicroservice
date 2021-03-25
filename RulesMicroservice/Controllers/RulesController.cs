386791006d29154db0a919fd53db90d0da5330dd
Suriyakumar-885061 <ssuriyakumarsvg@outlook.com>
2021-03-25 21:30:17 +0530

    first commit

---
+++ RulesMicroservice/Controllers/RulesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RulesMicroservice.Controllers
+{
+    //https://localhost:44374/api/rules
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RulesController : ControllerBase
+    {
+        [HttpGet]
+        public string EvaluateMinBal(double balance, int AccountId)
+        {
+            string ruleStatus = "Denied";
+            double minBalance = 1000;
+            if(balance < minBalance)
+            {
+                ruleStatus = "Denied";
+            }
+            else if(balance > minBalance)
+            {
+                ruleStatus = "Allowed";
+            }
+            return ruleStatus;
+        }
+
+        //[HttpGet]
+        //public float GetServiceCharges()
+        //{
+        //    float serviceCharge = 550;
+        //    return serviceCharge;
+        //}
+    }
+}
---
+++ RulesTest/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RulesMicroservice.Controllers;
+using NUnit.Framework;
+
+namespace RulesTest
+{
+    public class Class1
+    {
+        [Test]
+        public void EvaluateMinBal_InvalidData()
+        {
+            var balance = 500;
+            var accountid = 1001;
+            RulesController controller = new RulesController();
+            string result = controller.EvaluateMinBal(balance, accountid);
+            Assert.AreEqual(result,"Denied");
+        }
+
+        [Test]
+        public void EvaluateMinBal_validData()
+        {
+            var balance = 5500;
+            var accountid = 1001;
+            RulesController controller = new RulesController();
+            string result = controller.EvaluateMinBal(balance, accountid);
+            Assert.AreEqual(result, "Allowed");
+        }
+    }
+}
